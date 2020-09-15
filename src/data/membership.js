679f467051abda462f3d523ccd1e064ef7d85920
natalialexandra <natalia.alexandra195@gmail.com>
2020-09-15 13:05:40 +0200

    add data

---
+++ src/data/membership.js
@@ -0,0 +1,28 @@
+const membership = [
+    {
+        id: 1,
+        type: "Classic",
+        description: "",
+        period: "6 month contract",
+        price: 15.99,
+
+    },
+    {
+        id: 2,
+        type: "Premium",
+        description: "",
+        period: "1 year contract",
+        price: 14.99,
+        plus: "Bring-A-Friend every weekend"
+    },
+    {
+        id: 3,
+        type: "Flex",
+        description: "",
+        period: "1 month membership",
+        price: 17.99,
+
+    }
+]
+
+export default membership
---
+++ src/data/services.js
@@ -0,0 +1,52 @@
+const gymServices = [
+    {
+        id: 1,
+        type: "Endurance Training",
+        label: "",
+        text: "",
+        src: "./assets/endurance-training.png",
+        path: "/service"
+    },
+    {
+        id: 2,
+        type: "Weight Training",
+        label: "",
+        text: "",
+        src: "./assets/weight-training.png",
+        path: "/service"
+    },
+    {
+        id: 3,
+        type: "Stretching",
+        label: "",
+        text: "",
+        src: "./assets/stretching-training.png",
+        path: "/service"
+    },
+    {
+        id: 4,
+        type: "Personal Training",
+        label: "",
+        text: "",
+        src: "./assets/personal-training.png",
+        path: "/service"
+    },
+    {
+        id: 5,
+        type: "Group Training",
+        label: "",
+        text: "",
+        src: "./assets/group-training.png",
+        path: "/service"
+    },
+    {
+        id: 6,
+        type: "Online Live Training",
+        label: "",
+        text: "Workouts for every training goal and fitness level",
+        src: "./assets/online-training.png",
+        path: "/service"
+    },
+]
+
+export default gymServices
