917e7cbe0b82067551dbfb578469035c8e58a5db
natalialexandra <natalia.alexandra195@gmail.com>
2020-09-23 20:20:33 +0200

    home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import Cards from '../components/Cards'
+import Footer from '../components/Footer'
+
+export default function Home() {
+    return (
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
+    )
+}
