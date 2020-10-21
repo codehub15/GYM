f7c865966f949e452931a6b8706d6f3d3159a431
natalialexandra <natalia.alexandra195@gmail.com>
2020-10-21 20:48:10 +0200

    Updates

---
+++ precache-manifest.8d358b2a771d827d5a1cbf117f2a363a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0b6f7c75672a7a241694476c8880a5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "544594b9b9fda5444262",
+    "url": "./static/css/main.29f85267.chunk.css"
+  },
+  {
+    "revision": "8aecd3b3a3155e9122e9",
+    "url": "./static/js/2.321503e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.321503e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "544594b9b9fda5444262",
+    "url": "./static/js/main.fbe36a42.chunk.js"
+  },
+  {
+    "revision": "2bc20bb9523d1033a237",
+    "url": "./static/js/runtime-main.99ffbf97.js"
+  },
+  {
+    "revision": "4290131e01bd54247563afe47da086a2",
+    "url": "./static/media/men-figur.4290131e.png"
+  }
+]);
